52bf201edc3a7dbde205e692b528340234025407
Andrei Rubaniuk <rubaniuk@gmail.com>
2023-12-23 12:12:59 -0800

    Move output file into variable; TODOs.

--- WPLGenerator.cmd
+++ WPLGenerator.cmd
@@ -6,23 +6,38 @@
 ::
 @echo off
 setlocal EnableDelayedExpansion
+:: TODO: do robust input parameter validation
 
-echo ^<?xml version="1.0"?^> > Playlist.wpl
-echo ^<smil^> >> Playlist.wpl
-echo    ^<head^> >> Playlist.wpl
-echo        ^<meta name="Generator" content="WPL Generator v1.0"/^> >> Playlist.wpl
-echo        ^<title^>Playlist.wpl^</title^> >> Playlist.wpl
-echo    ^</head^> >> Playlist.wpl
-echo    ^<body^> >> Playlist.wpl
-echo        ^<seq^> >> Playlist.wpl
+:: TODO: get path to Photo/Video from command line and assign it to outputFile
+
+:: TODO: check if given path exists
+
+:: TODO: temporarily PUSHD into that directory
+
+
+set outputFile=Playlist.wpl
+
+echo ^<?xml version="1.0"?^> > %outputFile%
+echo ^<smil^> >> %outputFile%
+echo    ^<head^> >> %outputFile%
+echo        ^<meta name="Generator" content="WPL Generator v1.0"/^> >> %outputFile%
+echo        ^<title^>%outputFile%^</title^> >> %outputFile%
+echo    ^</head^> >> %outputFile%
+echo    ^<body^> >> %outputFile%
+echo        ^<seq^> >> %outputFile%
 
 for /f "tokens=*" %%G in ('dir /b /s *.mp4 *.mov') do (
-    echo        ^<media src="%%G"/^> >> Playlist.wpl 
+    :: TODO make %%G into a relative path so that when this folder is copied, playlist can be used
+    echo        ^<media src="%%G"/^> >> %outputFile% 
 )
 
-echo        ^</seq^> >> Playlist.wpl
-echo    ^</body^> >> Playlist.wpl
-echo ^</smil^> >> Playlist.wpl
+echo        ^</seq^> >> %outputFile%
+echo    ^</body^> >> %outputFile%
+echo ^</smil^> >> %outputFile%
+
+
+
+:: TODO: POPD from new working directory
 
 endlocal
 exit /b
